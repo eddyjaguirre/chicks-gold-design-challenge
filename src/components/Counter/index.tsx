2363aa80b7b02ce824dd5607fee381de89199d9b
Eddy Aguirre <eddyj.aguirre@gmail.com>
2022-12-07 19:42:54 -0400

    Fix Item counter

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,23 +1,12 @@
 import './style.scss';
-import { mdiMenuUp, mdiMenuDown } from '@mdi/js';
-import Icon from '@mdi/react';
 
 function Counter() {
   return (
     <div className='counter'>
-      <input type="number" />
-      {/* <div className='counter__buttons-container'>
-        <button>
-          <Icon path={mdiMenuUp}
-            size={0.7}
-          />
-        </button>
-        <button>
-          <Icon path={mdiMenuDown}
-            size={0.7}
-          />
-        </button>
-      </div> */}
+      <input
+        type="number"
+        value={1}
+      />
     </div>
   )
 }
