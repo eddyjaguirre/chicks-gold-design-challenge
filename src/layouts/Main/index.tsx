f548d2bd30fd81987840fd34d4436495fd1b2af8
Eddy Aguirre <eddyj.aguirre@gmail.com>
2022-12-07 19:38:09 -0400

    Add background image

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -1,9 +1,18 @@
 import './style.scss'
 import ItemsContainer from '../../components/ItemsContainer';
+import bg from '../../assets/osrs.webp';
 
 function Content() {
   return(
-    <div className="content">
+    <div
+      className="content"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top',
+        backgroundSize: '100vw auto'
+      }}
+    >
       <div className="container">
         <ItemsContainer
           title='Condimentum consectetur'
